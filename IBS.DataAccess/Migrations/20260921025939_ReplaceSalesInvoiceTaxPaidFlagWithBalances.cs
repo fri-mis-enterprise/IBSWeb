58c479aa7c8e7fd9758a6287223a25d97e672dc9
azhadolfo <azhadolfo1@gmail.com>
2026-09-22 13:56:11 +0800

    fix: add query in migration to compute the tax

--- IBS.DataAccess/Migrations/20260921025939_ReplaceSalesInvoiceTaxPaidFlagWithBalances.cs
+++ IBS.DataAccess/Migrations/20260921025939_ReplaceSalesInvoiceTaxPaidFlagWithBalances.cs
@@ -55,6 +55,37 @@ namespace IBS.DataAccess.Migrations
                 type: "numeric(18,4)",
                 nullable: false,
                 defaultValue: 0m);
+
+            migrationBuilder.Sql(
+                """
+                WITH invoice_tax AS
+                (
+                    SELECT
+                        invoice.sales_invoice_id,
+                        CASE WHEN COALESCE(cos.has_ewt, customer.with_holding_tax, false) THEN
+                            ROUND((CASE WHEN COALESCE(cos.vat_type, customer.vat_type) = 'Vatable'
+                                THEN ROUND((invoice.amount - invoice.discount) / 1.12, 4)
+                                ELSE ROUND(invoice.amount - invoice.discount, 4)
+                            END) * invoice.cwt_percent, 4)
+                        ELSE 0 END AS cwt_amount,
+                        CASE WHEN COALESCE(cos.has_wvat, customer.with_holding_vat, false) THEN
+                            ROUND((CASE WHEN COALESCE(cos.vat_type, customer.vat_type) = 'Vatable'
+                                THEN ROUND((invoice.amount - invoice.discount) / 1.12, 4)
+                                ELSE ROUND(invoice.amount - invoice.discount, 4)
+                            END) * invoice.cw_vat_percent, 4)
+                        ELSE 0 END AS cw_vat_amount
+                    FROM filpride_sales_invoices AS invoice
+                    INNER JOIN filpride_customers AS customer
+                        ON customer.customer_id = invoice.customer_id
+                    LEFT JOIN filpride_customer_order_slips AS cos
+                        ON cos.customer_order_slip_id = invoice.customer_order_slip_id
+                )
+                UPDATE filpride_sales_invoices AS invoice
+                SET cwt_balance = tax.cwt_amount,
+                    cw_vat_balance = tax.cw_vat_amount
+                FROM invoice_tax AS tax
+                WHERE tax.sales_invoice_id = invoice.sales_invoice_id;
+                """);
         }
 
         /// <inheritdoc />
--- IBS.DataAccess/Migrations/20260921080414_AddServiceInvoicePaymentBalances.cs
+++ IBS.DataAccess/Migrations/20260921080414_AddServiceInvoicePaymentBalances.cs
@@ -54,61 +54,18 @@ namespace IBS.DataAccess.Migrations
 
             migrationBuilder.Sql(
                 """
-                UPDATE filpride_collection_receipt_details AS detail
-                SET ewt = receipt.ewt,
-                    wvat = receipt.wvat
-                FROM filpride_collection_receipts AS receipt
-                WHERE detail.collection_receipt_id = receipt.collection_receipt_id
-                  AND receipt.service_invoice_id IS NOT NULL;
-
-                UPDATE filpride_service_invoices AS invoice
-                SET debit_amount = COALESCE((
-                        SELECT SUM(memo.debit_amount)
-                        FROM filpride_debit_memos AS memo
-                        WHERE memo.service_invoice_id = invoice.service_invoice_id
-                          AND memo.status = 'Posted'
-                          AND memo.posted_by IS NOT NULL
-                    ), 0),
-                    credit_amount = COALESCE((
-                        SELECT SUM(ABS(memo.credit_amount))
-                        FROM filpride_credit_memos AS memo
-                        WHERE memo.service_invoice_id = invoice.service_invoice_id
-                          AND memo.status = 'Posted'
-                          AND memo.posted_by IS NOT NULL
-                    ), 0);
-
-                UPDATE filpride_service_invoices AS invoice
-                SET cwt_amount_paid = COALESCE((
-                        SELECT SUM(detail.ewt)
-                        FROM filpride_collection_receipt_details AS detail
-                        INNER JOIN filpride_collection_receipts AS receipt
-                            ON receipt.collection_receipt_id = detail.collection_receipt_id
-                        WHERE receipt.service_invoice_id = invoice.service_invoice_id
-                          AND detail.invoice_no = invoice.service_invoice_no
-                          AND receipt.status NOT IN ('Canceled', 'Voided')
-                    ), 0),
-                    cw_vat_amount_paid = COALESCE((
-                        SELECT SUM(detail.wvat)
-                        FROM filpride_collection_receipt_details AS detail
-                        INNER JOIN filpride_collection_receipts AS receipt
-                            ON receipt.collection_receipt_id = detail.collection_receipt_id
-                        WHERE receipt.service_invoice_id = invoice.service_invoice_id
-                          AND detail.invoice_no = invoice.service_invoice_no
-                          AND receipt.status NOT IN ('Canceled', 'Voided')
-                    ), 0);
-
                 UPDATE filpride_service_invoices AS invoice
                 SET cwt_balance = CASE WHEN invoice.has_ewt THEN
                         ROUND((CASE WHEN invoice.vat_type = 'Vatable'
-                            THEN (invoice.total - invoice.discount + invoice.debit_amount - invoice.credit_amount) / 1.12
-                            ELSE invoice.total - invoice.discount + invoice.debit_amount - invoice.credit_amount
-                        END) * (invoice.service_percent / 100), 4) - invoice.cwt_amount_paid
+                            THEN ROUND((invoice.total - invoice.discount) / 1.12, 4)
+                            ELSE ROUND(invoice.total - invoice.discount, 4)
+                        END) * (invoice.service_percent / 100), 4)
                     ELSE 0 END,
                     cw_vat_balance = CASE WHEN invoice.has_wvat THEN
                         ROUND((CASE WHEN invoice.vat_type = 'Vatable'
-                            THEN (invoice.total - invoice.discount + invoice.debit_amount - invoice.credit_amount) / 1.12
-                            ELSE invoice.total - invoice.discount + invoice.debit_amount - invoice.credit_amount
-                        END) * 0.05, 4) - invoice.cw_vat_amount_paid
+                            THEN ROUND((invoice.total - invoice.discount) / 1.12, 4)
+                            ELSE ROUND(invoice.total - invoice.discount, 4)
+                        END) * 0.05, 4)
                     ELSE 0 END;
                 """);
         }
